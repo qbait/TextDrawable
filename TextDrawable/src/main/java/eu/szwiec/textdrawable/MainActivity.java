c9a41bfe53ac1e045caddaa2dbf6cc31a7e08ee3
qbait <szwiec@MacBook-Pro-Jakub.local>
2014-01-28 23:35:53 +0100

    Initial commit

---
+++ TextDrawable/src/main/java/eu/szwiec/textdrawable/MainActivity.java
@@ -0,0 +1,27 @@
+package eu.szwiec.textdrawable;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.ImageView;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        TextDrawable textDrawable = new TextDrawable(this);
+        textDrawable.setText("a");
+        imageView.setImageDrawable(textDrawable);
+    }
+}
